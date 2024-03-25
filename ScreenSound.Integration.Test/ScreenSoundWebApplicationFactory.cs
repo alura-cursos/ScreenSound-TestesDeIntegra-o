b5d300b39542a4a38bf8fce286a41880e6b7dbca
bessax <andrebessax@gmail.com>
2024-03-25 13:06:07 -0300

    #Update: Ajustes.

--- ScreenSound.Integration.Test/ScreenSoundWebApplicationFactory.cs
+++ ScreenSound.Integration.Test/ScreenSoundWebApplicationFactory.cs
@@ -6,15 +6,7 @@ namespace ScreenSound.Integration.Test
 {
     public class ScreenSoundWebApplicationFactory: WebApplicationFactory<Program>
     {
-        public ScreenSoundContext Context { get; }
-
-        private IServiceScope scope;
-
-        public ScreenSoundWebApplicationFactory()
-        {
-            this.scope = Services.CreateScope();
-            Context = scope.ServiceProvider.GetRequiredService<ScreenSoundContext>();
-        }
+        
 
     }
 }
